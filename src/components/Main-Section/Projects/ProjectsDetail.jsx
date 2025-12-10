06fb8adfd5377135c077643bd16ce30381048e04
KINGALVI <alvialmahamud@gmail.com>
2025-12-10 19:51:41 +0600

    edited some code

--- src/components/Main-Section/Projects/ProjectsDetail.jsx
+++ src/components/Main-Section/Projects/ProjectsDetail.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { useParams, Link } from "react-router-dom";
-import { PortfolioContextAPI } from "../../../Context-Api/Portfolio-Context";
+import { PortfolioContextAPI } from "../../../Context-API/PortfolioContext";
 
 const ProjectsDetail = () => {
     const { id } = useParams();
