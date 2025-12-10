262844210e42e39ebc667ddecc8b77409cdd5e1a
KINGALVI <alvialmahamud@gmail.com>
2025-12-10 20:59:22 +0600

    edited some code

--- src/components/Main-Section/Home/Home.jsx
+++ src/components/Main-Section/Home/Home.jsx
@@ -43,7 +43,6 @@ const Home = () => {
                 </div>
             </section>
 
-            {/* Quote Section */}
             <section className="flex justify-center">
                 <div>
                     <img className="reletive-quote_man-image" src={quote_man} alt="" />
