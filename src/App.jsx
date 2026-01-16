91cb9b42a5ae2db4bcd4046d458fd2a852835790
KINGALVI <alvialmahamud@gmail.com>
2026-01-16 07:05:28 +0600

    edited some code

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
   // Refresh AOS after JSON data loads
   useEffect(() => {
     if (LargeProjectsAPI.length && SmallProjectsAPI.length) {
-      AOS.refresh();
+      AOS.refreshHard();
     }
   }, [LargeProjectsAPI, SmallProjectsAPI]);
 
